5891cc72fe27894f9276ac64222b01534613ed20
unknown <valikpalica>
2021-05-21 18:01:57 +0300

    functional person done

--- API_DB/AdminMethod/blockCountry.js
+++ API_DB/AdminMethod/blockCountry.js
@@ -1,9 +1,9 @@
 const mysql = require('../connection');
 
 const blockCountry = (obj) =>{
-    let {country_id,peron_id,status} = obj;
+    let {country_id,person_id,status} = obj;
     return new Promise((res,rej)=>{
-        mysql.query('insert into enable (country_id,peron_id,status) values (?,?,?)',[country_id,peron_id,status],(err,result) =>{
+        mysql.query('insert into enable (country_id,person_id,status) values (?,?,?)',[country_id,person_id,status],(err,result) =>{
             if(err){
                 rej(new Error(err));
             }
--- public/js/InformationPerson.js
+++ public/js/InformationPerson.js
@@ -14,7 +14,16 @@ document.addEventListener('DOMContentLoaded',(event)=>{
         createMainInfo(response.person[0]);
         createCardCountry(response.countrys);
     }).then(()=>{
-
+        fetch('/admin/allCountry').then(response=>{
+                return response.json();
+        }).then(data=>{
+            visible('visibleAdd');
+            visible('visibleblock');
+            let countryAdd = document.getElementById('countryAddselector');
+            countryAdd.append(createSelector(data,'addTravel'));
+            let countryblock = document.getElementById('countryBlock');
+            countryblock.append(createSelector(data,'blockBountry'));
+        })
     })
     .catch(err=>{console.log(err)})
 })
@@ -53,3 +62,72 @@ const createCardCountry = (countrys) =>{
     }
 } 
 
+const createSelector = (countrys,parametrs) =>{
+    let selector = document.createElement('select');
+    selector.className = 'form-select';
+    selector.id = parametrs;
+    countrys.forEach(element => {
+        let option= document.createElement('option');
+        option.value  = element.id_country;
+        option.textContent = element.name;
+        selector.append(option);
+    });
+    return selector;
+};
+
+const visible  = (parametrs) =>{
+    let country = document.getElementById(parametrs);
+    country.hidden = country.hidden == true ? false :true; 
+}
+
+document.getElementById('addtr').addEventListener('click',()=>{
+    visible('visibleAdd');
+});
+document.getElementById('addbl').addEventListener('click',()=>{
+    visible('visibleblock')
+});
+
+
+const Save = (parametrs,callback) =>{
+    let selector = document.getElementById(parametrs);
+    let value = selector.options[selector.selectedIndex].value;
+    console.log(value);
+    callback(value);
+}
+
+const response = (url,body) =>{
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(body),
+    }).then(data=>{
+        alert('DONE');
+        window.location.href = `/page/infoPerson/${document.getElementById('id').value}`
+    }).catch(err=>{
+        alert('err');
+    })
+}
+
+document.getElementById('saveTravel').addEventListener('click',()=>{
+    Save('addTravel',(index)=>{
+        let date = document.getElementById('date_travel').value;
+        let obj = {}
+        obj['country_id'] = index;
+        obj['date'] = date;
+        obj['peron_id'] = document.getElementById('id').value; 
+        console.log(obj)
+        response('/admin/addTravel',obj);
+    });
+})
+document.getElementById('blockTravel').addEventListener('click',()=>{
+    Save('blockBountry',(index)=>{
+        let obj  = {}
+        obj['country_id'] = index;
+        obj['person_id'] = document.getElementById('id').value; 
+        obj['status']  = 'block';
+        console.log(obj) 
+        response('/admin/blockCountry',obj);
+    });
+})
